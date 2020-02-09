3ff3f6902e8df3d9054eabcba9965ad909ec8571
brettmadrid <brettmadrid9@gmail.com>
2020-02-08 21:48:54 -0800

    finished miner - MVP

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -23,10 +23,13 @@ def proof_of_work(last_proof):
     start = timer()
 
     print("Searching for next proof")
-    proof = 0
-    #  TODO: Your code here
+    proof = random.randint(-2147483647, 2147483647)
+    last_hash = hashlib.sha3_256(str(last_proof).encode()).hexdigest()
+    while valid_proof(last_hash, proof) is False:
+        proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
+    print("Proof hash is: ", hashlib.sha3_256(str(proof).encode()).hexdigest())
     return proof
 
 
@@ -40,7 +43,9 @@ def valid_proof(last_hash, proof):
     """
 
     # TODO: Your code here!
-    pass
+    guess = hashlib.sha256(str(proof).encode()).hexdigest()
+
+    return guess[:6] == last_hash[-6:]
 
 
 if __name__ == '__main__':
